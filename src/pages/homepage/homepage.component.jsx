b1e1240ab02dbe01e6bcf71cb7bbd16e4f01d665
tucker-green <88201589+tucker-green@users.noreply.github.com>
2021-08-02 23:45:37 -0400

    broken homepage into components and changes style up to add images, transitions and more

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import { div } from "prelude-ls";
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
